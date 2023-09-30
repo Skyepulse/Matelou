63a971cd6b71ef3469b498d37baad2f672e8a986
Telo <telo.philippe@telecom-paris.fr>
2023-09-30 13:04:23 +0200

    Refactored variable names and add facing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Collider2D))]
@@ -11,50 +11,59 @@ public class PlayerMovement : MonoBehaviour {
     public float jumpHeight = 10.0f;
     public float gravityScale = 1.5f;
 
-    private Transform t;
-	private Rigidbody2D rb2D;
-	private Collider2D mainCollider;
+    private Transform _transform;
+	private Rigidbody2D _rb2D;
+	private Collider2D _mainCollider;
 
-    private bool isGrounded = false;
-    private bool facingRight = true;
-    private float velX = 0.0f;
+    private bool _isGrounded = false;
+    private bool _facingRight = true;
+    private float _velX = 0.0f;
 
 
     void Start() {
-        t = transform;
-        rb2D = GetComponent<Rigidbody2D>();
-        mainCollider = GetComponent<Collider2D>();
+        _transform = transform;
+        _rb2D = GetComponent<Rigidbody2D>();
+        _mainCollider = GetComponent<Collider2D>();
 
-        rb2D.freezeRotation = true;
-        rb2D.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
-        rb2D.gravityScale = gravityScale;
+        _rb2D.freezeRotation = true;
+        _rb2D.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        _rb2D.gravityScale = gravityScale;
     }
 
     void Update() {
-        velX = Input.GetAxis("Horizontal");
+        _velX = Input.GetAxis("Horizontal");
+        if(Mathf.Abs(_velX) > 0.01f) {
+            if(_velX > 0) {
+                _facingRight = true;
+                _transform.localScale = new Vector3(Mathf.Abs(_transform.localScale.x), _transform.localScale.y, _transform.localScale.z);
+            } else {
+                _facingRight = false;
+                _transform.localScale = new Vector3(-Mathf.Abs(_transform.localScale.x), _transform.localScale.y, _transform.localScale.z);
+            }
+        }
 
-        if(isGrounded && Input.GetKey(KeyCode.Space)) {
-            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpHeight);
+        if(_isGrounded && Input.GetKey(KeyCode.Space)) {
+            _rb2D.velocity = new Vector2(_rb2D.velocity.x, jumpHeight);
         }
     }
 
 	private void FixedUpdate() {
-        Bounds colliderBounds = mainCollider.bounds;
+        Bounds colliderBounds = _mainCollider.bounds;
         Vector2 pointA = colliderBounds.min;
         Vector2 pointB = pointA + new Vector2(colliderBounds.max.x - colliderBounds.min.x, -0.1f);
 
         Collider2D[] colliders = Physics2D.OverlapAreaAll(pointA, pointB);
 
 
-        isGrounded = false;
+        _isGrounded = false;
 
         foreach(Collider2D col in colliders) {
-			if (col != mainCollider) {
-				isGrounded = true;
+			if (col != _mainCollider) {
+				_isGrounded = true;
 				//break;
 			}
 		}
 
-		rb2D.velocity = new Vector2(velX * maxSpeed, rb2D.velocity.y);
+		_rb2D.velocity = new Vector2(_velX * maxSpeed, _rb2D.velocity.y);
 	}
 }
